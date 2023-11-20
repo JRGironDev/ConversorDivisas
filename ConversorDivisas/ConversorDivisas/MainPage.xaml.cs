13c06b1d337430eee3846837fc71a7815e5f0503
Jose Quiñónez <joserobertoq@outlook.com>
2023-11-20 07:23:07 -0600

    First Commit

--- ConversorDivisas/ConversorDivisas/MainPage.xaml.cs
+++ ConversorDivisas/ConversorDivisas/MainPage.xaml.cs
@@ -10,9 +10,44 @@ namespace ConversorDivisas
 {
     public partial class MainPage : ContentPage
     {
+        double q;
+
+        double dolares;
+
+        double factor = 8;
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private void Convertir()
+        {
+            q = Convert.ToDouble(inputQuetzales.Text);
+            dolares = q / factor;
+            if(dolares != 1)
+            {
+                lblResultado.Text = dolares.ToString() + " dólares";
+            } else
+            {   
+                lblResultado.Text = dolares.ToString() + " dolar";
+            }            
+        }
+
+        private void Validar()
+        {
+            if (!string.IsNullOrEmpty(inputQuetzales.Text))
+            {
+                Convertir();
+            } else
+            {
+                DisplayAlert("Error", "Debe ingresar un monto en quetzales valido", "Aceptar");
+                inputQuetzales.Focus();
+            }
+        }
+
+        private void btnConvertir_Clicked(object sender, EventArgs e)
+        {
+            Validar();
         }
     }
 }
